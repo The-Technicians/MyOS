4c959fcdd0ce777c2f0e9fb119240caff33e229d
Programmer <45080506+jaKjH101@users.noreply.github.com>
2020-03-30 17:27:10 -0500

    Update Boot.asm

--- Boot.asm
+++ Boot.asm
@@ -2,25 +2,25 @@ BITS 16
 jmp short bootloader_start
 nop
 
-OEMLabel                  db "TINKERBOOT"
-BytesPerSector            dw 512
-SectorPerCluster          db 1
-ReservedForBoot           dw 1
-NumberOfFats              db 2
-RootDirEntries            dw 224
+OEMLabel		db "TINKERBOOT"
+BytesPerSector		dw 512
+SectorPerCluster		db 1
+ReservedForBoot		dw 1
+NumberOfFats		db 2
+RootDirEntries		dw 224
 
-LogicalSectors            dw 2880
-MediumByte                db 0F0h
-SectorsPerFat             dw 9
-SectorsPerTrack           dw 18
-Slides                    dw 2
-HiddenSectors             dd 0
-LargeSectors							dd 0
-DriveNo										dw 0
-Signature									db 41
-VolumeID									dd 00000000h
-VolumeLable								db "TINKEROS"
-FileSystem								db "FAT12"
+LogicalSectors		dw 2880
+MediumByte		db 0F0h
+SectorsPerFat		dw 9
+SectorsPerTrack		dw 18
+Slides		dw 2
+HiddenSectors		dd 0
+LargeSectors		dd 0
+DriveNo		dw 0
+Signature		db 41
+VolumeID		dd 00000000h
+VolumeLable		db "TINKEROS"
+FileSystem		db "FAT12"
 
 
 Bootloader_start:
